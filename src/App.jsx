6361521888cf60fb0b6c7487df75588b4ae5c984
Preslav Getov <preslav.getov@yara.com>
2023-03-23 14:45:30 +0200

    axios works with params

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import GlobalStyleProvider from "./components/GlobalStyleProvider";
 const App = () => {
-  const [count, setCount] = useState(0);
+  const baseReqUrl = "https://www.omdbapi.com/?t=inception&apikey=6f309716";
+  const baseReqUrlNoParams = "https://www.omdbapi.com/";
+  const getData = async () => {
+    try {
+      const rawData = await axios.get(baseReqUrlNoParams, {
+        params: {
+          t: "inception",
+          apikey: "6f309716",
+        },
+      });
+      console.log(rawData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const [data, setData] = useState({});
 
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <>
       <GlobalStyleProvider />
