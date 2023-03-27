6aa3b66341586bef9622dc29e17db1dc25245c7c
Preslav Getov <preslav.getov@yara.com>
2023-03-27 14:20:58 +0300

    remove works

--- src/routes/watchlist/Watchlist.jsx
+++ src/routes/watchlist/Watchlist.jsx
@@ -2,15 +2,21 @@ import React from "react";
 import { UlStyles, LiStyles, WatchlistContainer } from "./watchlist.style.css";
 import ActionableButton from "../../components/shared/ActionableButton";
 import MovieElement from "../../components/movie/Movie";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { removeWatchlistItem } from "../../components/slices/watchlistSlice";
 const Watchlist = () => {
   const watchlistItems = useSelector((state) => state.watchlist.value);
+  const dispatch = useDispatch();
   console.log(watchlistItems);
   return (
     <div>
       {watchlistItems.map((singleItem) => (
         <MovieElement key={singleItem.id} title={singleItem.title}>
-          <ActionableButton title="Submit me" />
+          <ActionableButton
+            title="Remove"
+            backgroundColor="red"
+            onClick={() => dispatch(removeWatchlistItem(singleItem.id))}
+          />
         </MovieElement>
       ))}
     </div>
